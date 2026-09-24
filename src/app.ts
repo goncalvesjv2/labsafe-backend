c29e3f0d3ce0555c83c67d4504a2c550c61d3eeb
goncalvesjv2 <joaogoncalves3m@gmail.com>
2026-09-24 09:20:11 -0300

    feat: adicionando o plugin JWT na aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
+import "dotenv/config";
 import fastifyCors from "@fastify/cors";
 import Fastify from "fastify";
 import { userRoutes } from "./routes/userRoutes";
 import { authRoutes } from "./routes/authRoutes";
+import fastifyJwt from "@fastify/jwt";
 
 export const app = Fastify({ logger: true });
 
 app.register(fastifyCors, {
     origin: "*",
+})
+
+const secretJWT = process.env.JWT_SECRET;
+
+if (!secretJWT) {
+    throw new Error("Chave secreta do JWT não configurado")
+}
+
+app.register(fastifyJwt, {
+    secret: secretJWT,
 })
 
 app.register(userRoutes, { prefix: '/api' });
