5e69e2c5785d4b746a474b02244fc4b73b0276ff
goncalvesjv2 <joaogoncalves3m@gmail.com>
2026-09-26 12:05:33 -0300

    feat: adicionando proteção nas rotas de usuários em todos os métodos HTTP

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,8 +5,8 @@ import { authorizationMiddleware } from "../middlewares/authorizationMiddleware"
 
 export const userRoutes = async (app: FastifyInstance) => {
     app.get("/users", {preHandler: [authMiddleware, authorizationMiddleware]},listUsersController);
-    app.get<{Params: {id: number}}>("/users/:id", listUserByIdController);
-    app.post("/users", createUserController);
-    app.patch<{Params: {id: number}}>("/users/:id", updateUserController);
-    app.delete<{Params: {id: number}}>("/users/:id", deleteUserController);
+    app.get<{Params: {id: number}}>("/users/:id", {preHandler: [authMiddleware, authorizationMiddleware]},listUserByIdController);
+    app.post("/users", {preHandler: [authMiddleware, authorizationMiddleware]} ,createUserController);
+    app.patch<{Params: {id: number}}>("/users/:id", {preHandler: [authMiddleware, authorizationMiddleware]}, updateUserController);
+    app.delete<{Params: {id: number}}>("/users/:id", {preHandler: [authMiddleware, authorizationMiddleware]}, deleteUserController);
 }
